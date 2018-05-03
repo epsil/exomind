b5cf6fe980e80ed3d28e211fa024f7ec7c7c50e6
Vegard Øye <vegard_oye@hotmail.com>
2018-05-03 13:09:47 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const body = document.body;
+// const div = document.createElement('div');
+// body.appendChild(div);
+// ReactDOM.render(<App />, div);
+ReactDOM.render(<App />, body);
+
 registerServiceWorker();
