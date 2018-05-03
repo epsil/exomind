80d7138eb7461899d0fca4c402b7ed5ee5e49f7e
Vegard Øye <vegard_oye@hotmail.com>
2018-05-03 15:12:43 +0200

    Fix warning

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from './registerServiceWorker';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 const body = document.body;
-// const div = document.createElement('div');
-// body.appendChild(div);
-// ReactDOM.render(<App />, div);
-ReactDOM.render(<App />, body);
+const div = document.createElement('div');
+body.appendChild(div);
+ReactDOM.render(<App />, div);
+// ReactDOM.render(<App />, body);
 
 registerServiceWorker();
