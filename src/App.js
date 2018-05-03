d63dd2d0e5035ab0d862710a859df952b30c7f84
Vegard Øye <vegard_oye@hotmail.com>
2018-05-03 13:15:58 +0200

    Run prettier on App.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,32 @@ import fetch from 'isomorphic-fetch';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countryNames: [],
+      loading: false
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            countryNames: [],
-            loading: false
-        }
-    }
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch('https://restcountries.eu/rest/v1/all')
+      .then(response => response.json())
+      .then(json => json.map(country => country.name))
+      .then(countryNames => this.setState({ countryNames, loading: false }));
+  }
 
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch('https://restcountries.eu/rest/v1/all')
-            .then(response => response.json())
-            .then(json => json.map(country => country.name))
-            .then(countryNames =>
-                  this.setState({countryNames, loading: false})
-                 )
-    }
-
-    render() {
-        const { countryNames, loading } = this.state
-        return (loading) ?
-            <div>Loading Country Names...</div> :
-            (!countryNames.length) ?
-                <div>No country Names</div> :
-                <ul>
-                    {countryNames.map(
-                        (x,i) => <li key={i}>{x}</li>
-                    )}
-                </ul>
-    }
+  render() {
+    const { countryNames, loading } = this.state;
+    return loading ? (
+      <div>Loading Country Names...</div>
+    ) : !countryNames.length ? (
+      <div>No country Names</div>
+    ) : (
+      <ul>{countryNames.map((x, i) => <li key={i}>{x}</li>)}</ul>
+    );
+  }
 }
 
 export default App;
